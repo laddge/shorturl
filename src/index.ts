bbbae9e379c0921fc5c3e6378aa54858564d4385
Laddge <dev.laddge@gmail.com>
2025-05-20 17:29:10 +0900

    feat: リダイレクトの設定

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import { Hono } from 'hono'
+import { load } from 'js-toml'
+import urlMap from './url.toml'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+Object.entries(load(urlMap)).map(([route, config]) => {
+  app.get(`/${route}`, (c) => {
+    c.header('Location', config.url)
+    return c.html(`<html><body><a href="${config.url}">moved here</a></body></html>`, 301)
+  })
 })
 
 export default app
